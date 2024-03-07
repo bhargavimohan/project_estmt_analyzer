0c0300abb40ad69f63e6a6f75e3c523f2a3e51ad
bimn <bhargavi.mohan@ix-acn.com>
2024-03-07 22:57:17 +0100

    [UPDATE] ESA v1_frontend

--- src/app/pages/analyzer/analyzer.component.spec.ts
+++ src/app/pages/analyzer/analyzer.component.spec.ts
@@ -1,23 +1,75 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { Router, RouterOutlet, RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
-import { AnalyzerComponent } from './analyzer.component';
+@Component({
+  selector: 'app-analyzer',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink],
+  // providers : [HttpClient],
+  templateUrl: './analyzer.component.html',
+  styleUrl: './analyzer.component.css'
+})
 
-describe('AnalyzerComponent', () => {
-  let component: AnalyzerComponent;
-  let fixture: ComponentFixture<AnalyzerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AnalyzerComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(AnalyzerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+export class AnalyzerComponent {
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  selectedFile: File | null = null;
+  uploadSuccessMessage: string = '';
+  errorMessage: string = '';
+  successMessage: string = '';
+  uploadInProgress: boolean = false; 
+  uploadFailMessage : string = ''
+  
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file && !file.type.includes('pdf')) {
+      this.selectedFile = null;
+      this.errorMessage = 'Error: Please select a PDF file';
+    } else {
+      this.selectedFile = file;
+      this.errorMessage = '';
+    }
+  }
+
+  constructor(private http: HttpClient) {}
+
+  onUpload(event: any) {
+    if (!this.selectedFile) {
+      this.errorMessage = 'Error: No file selected';
+      return;
+    }
+    this.uploadInProgress = true;
+    {
+  
+      const formData: FormData = new FormData();
+      formData.append('file', this.selectedFile);
+      // Handle sucess response here
+      this.http.post('http://127.0.0.1:8001/receive', formData)
+        .subscribe((response : any) => {
+          console.log('File uploaded successfully:', response);
+          this.successMessage = 'File has been uploaded successfully';
+          this.uploadInProgress = false;
+        }, (error : any) => {
+          console.error('Error uploading file:', error);
+          this.uploadFailMessage = 'Upload error, Try again after sometime . . .'
+          this.successMessage = '';
+          this.uploadInProgress = false; 
+          // Handle error response here
+        });
+    }
+}
+
+onReset(event: any) {
+  this.selectedFile = null;
+  this.errorMessage = '';
+  this.successMessage = '';
+  this.uploadFailMessage = ''
+  const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+  if (fileInput) {
+    fileInput.value = ''; // Clear the selected file
+}
+}
+}
