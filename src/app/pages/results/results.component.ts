b5534d2e5c6242d8a3b2fba373554f934cb1ccc7
bhargavimohan <44068920+bhargavimohan@users.noreply.github.com>
2024-03-12 14:09:48 +0100

    [UPDATE] results rendering v1

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -2,14 +2,62 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterOutlet, RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [CommonModule, RouterOutlet , RouterLink],
+  imports: [CommonModule, RouterOutlet , RouterLink, FormsModule],
   templateUrl: './results.component.html',
   styleUrl: './results.component.css'
 })
-export class ResultsComponent{
+export class ResultsComponent implements OnInit {
+  fileNames: string[] = [];
+  fileDescription: { key: string, value: string }[] = [];
+  items: { [fileName: string]: any } = {}; // Dictionary to store file name and its corresponding JSON data
+  fileSelected : string = ''
+  specificKey: string = 'status_message';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getAllItems();
+    this.fileSelected = ''
+
+  }
+
+  getAllItems() {
+    this.http.get<any>('http://127.0.0.1:8001/analyzed-pdfs').subscribe(
+      data => {
+        this.items = data.items;
+        this.fileNames = Object.keys(this.items).map(fileName => Object.keys(this.items[fileName])[0]);
+      },
+
+      error => {
+        console.error('Error fetching items:', error);
+      }
+    );
+  }
+
+  onSelectFileName(fileName: string) {
+    if (fileName != null) {
+      const keys = Object.keys(this.items);
+      for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        if (this.items[key].hasOwnProperty(fileName)) {
+          const jsonString = this.items[key][fileName];
+          if (jsonString) {
+            this.fileDescription = JSON.parse(jsonString); // Ensure fileDescription is an array of objects
+          } else {
+            this.fileDescription = [];
+          }
+          return;
+        }
+      }
+      this.fileDescription = [];
+    } else {
+      this.fileDescription = [];
+    }
+  }
 }
