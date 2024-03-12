cabd1d7879facc0253eb1dae4c9c2282984a9d9f
bhargavimohan <44068920+bhargavimohan@users.noreply.github.com>
2024-03-12 16:25:14 +0100

    [UPDATE] display formatting

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -12,12 +12,15 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './results.component.html',
   styleUrl: './results.component.css'
 })
+
+
 export class ResultsComponent implements OnInit {
   fileNames: string[] = [];
   fileDescription: { key: string, value: string }[] = [];
   items: { [fileName: string]: any } = {}; // Dictionary to store file name and its corresponding JSON data
+  excludedItems: { key: string, value: string }[] = [];
   fileSelected : string = ''
-  specificKey: string = 'status_message';
+  excludedKeys: string[] = ['status_message', 'new_balance_costs', 'total_categories_costs'];
 
   constructor(private http: HttpClient) { }
 
@@ -48,16 +51,22 @@ export class ResultsComponent implements OnInit {
         if (this.items[key].hasOwnProperty(fileName)) {
           const jsonString = this.items[key][fileName];
           if (jsonString) {
-            this.fileDescription = JSON.parse(jsonString); // Ensure fileDescription is an array of objects
+            this.fileDescription = JSON.parse(jsonString)
+            this.excludedItems = Object.keys(JSON.parse(jsonString))
+            .filter(key => this.excludedKeys.includes(key))
+            .map(key => ({ key, value: JSON.parse(jsonString)[key] }))
           } else {
             this.fileDescription = [];
+            this.excludedItems = [];
           }
           return;
         }
       }
       this.fileDescription = [];
+      this.excludedItems = [];
     } else {
       this.fileDescription = [];
+      this.excludedItems = [];
     }
   }
 }
